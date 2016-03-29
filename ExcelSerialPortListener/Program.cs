a635ba4d1997f1572caab5bb00a66b5bafadd731
Steve Buckley <buckleys78@gmailcom>
2016-03-29 17:52:41 -0400

    Listener returns valid numbers from scale.

--- ExcelSerialPortListener/Program.cs
+++ ExcelSerialPortListener/Program.cs
@@ -7,14 +7,51 @@ using System.Threading.Tasks;
 
 namespace ExcelSerialPortListener {
     class Program {
+        static string response { get; set; }
+
         static void Main(string[] args) {
             CommChannel scaleComms = new CommChannel("COM3", "19200", "8", "One", "None");
             bool commsAreOpen = scaleComms.OpenPort();
             Console.WriteLine($"Comms are open = {scaleComms.IsOpen}");
-            Console.ReadKey();
+            //Console.ReadKey();
+            if (commsAreOpen)
+                Console.WriteLine($"Scale response: {scaleComms.ReadData()}");
             scaleComms.ClosePort();
             Console.WriteLine($"and now Comms are open = {scaleComms.IsOpen}");
             Console.ReadKey();
         }
+
+        //public static void Main() {
+
+        //    SerialPort mySerialPort = new SerialPort("COM3");
+
+        //    mySerialPort.BaudRate = 19200;
+        //    mySerialPort.Parity = Parity.None;
+        //    mySerialPort.StopBits = StopBits.One;
+        //    mySerialPort.DataBits = 8;
+        //    //mySerialPort.Handshake = Handshake.None;
+        //    //mySerialPort.RtsEnable = true;
+
+        //    mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+        //    mySerialPort.Open();
+
+        //    Console.WriteLine("Press the scale's Print button...");
+        //    Console.WriteLine();
+        //    Console.ReadKey();
+
+        //    Console.WriteLine($".DataReceived = {response}");
+        //    mySerialPort.Close();
+        //    Console.ReadKey();
+        //}
+
+        //private static void DataReceivedHandler(
+        //                    object sender,
+        //                    SerialDataReceivedEventArgs e) {
+        //    SerialPort sp = (SerialPort)sender;
+        //    response = sp.ReadExisting();
+        //    Console.WriteLine("Data Received:");
+        //    Console.Write(response);
+        //}
     }
 }
